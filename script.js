3091a85e668091febbd63e6f5f1e9e4face727ac
shabeeh9633 <muhammedsabeeh000@gmail.com>
2025-08-09 01:24:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// document.getElementById('contact-form').addEventListener('submit', function(event) {
+//   event.preventDefault();
+
+//   const serviceID = 'service_i7j4uru';
+//   const templateID = 'template_c24hwce';
+
+//   emailjs.sendForm(serviceID, templateID, this)
+//     .then(() => {
+//       alert('Sent!');
+//     }, (err) => {
+//       alert(JSON.stringify(err));
+//     });
+// });
